a9143a022fe8de85d2ab1a881619f58cee60103a
cyrenedelmundo <0323-2042@lspu.edu.ph>
2025-05-01 16:41:55 +0800

    "SD FINALS"

--- Photobooth.py
+++ Photobooth.py
@@ -16,6 +16,7 @@ from kivy.metrics import dp
 from kivy.graphics import Color, Rectangle
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.image import Image
+from kivymd.uix.list import OneLineListItem
 
 
 class ArrowButton(Button):
@@ -45,11 +46,18 @@ class HomePage(Screen):
 class CameraPage(Screen):
     pass
 
+
 class AccountPage(Screen):
     pass
 
+
 class AboutPage(Screen):
     pass
+
+
+class ContactPage(Screen):
+    pass
+
 
 KV = '''
 ScreenManager:
@@ -61,7 +69,9 @@ ScreenManager:
     CameraPage:
     AccountPage:
     AboutPage:
-    
+    ContactPage:
+
+
 <ArrowButton@Button>:
     size_hint: None, None
     size: 50, 50
@@ -393,18 +403,19 @@ ScreenManager:
 
                     Widget:
 
-                    MDBoxLayout:
+                    MDFloatLayout:
                         size_hint_y: None
                         height: dp(56)
                         padding: dp(10)
                         pos_hint: {"top": 1, "right": 1}  
-                        spacing: dp(10) 
+                        spacing: dp(10)
 
                         MDIconButton:
                             icon: "menu"
                             theme_icon_color: "Custom"
                             icon_color: "black"
-                            on_release: nav_drawer.set_state("toggle")  
+                            pos_hint: {"center_y": 13.20, "center_x": 0.95} 
+                            on_release: nav_drawer.set_state("toggle")             
 
                 ClickableImage:
                     source: "set1 (2).png"
@@ -412,34 +423,34 @@ ScreenManager:
                     size: dp(300), dp(300)
                     pos_hint: {"center_y": 0.35, "x": 0.10}
                     on_release: app.root.current = 'camera'
-                
+
                 MDLabel:
                     text: "SET 1"
                     halign: "center"
                     pos_hint: {"center_x": 0.20, "center_y": 0.15}
                     text_style: "GlacialIndifference-Regular.ttf"
-                         
-                             
+
+
                 ClickableImage:
                     source: "set2.png"
                     size_hint: None, None
                     size: dp(350), dp(350)
                     pos_hint: {"center_y": 0.38, "x": 0.25}
                     on_release: app.root.current = 'camera'
-                
+
                 MDLabel:
                     text: "SET 2"
                     halign: "center"
                     pos_hint: {"center_x": 0.36, "center_y": 0.15}
                     text_style: "GlacialIndifference-Regular.ttf"
-                    
+
                 ClickableImage:
                     source: "set3.png"
                     size_hint: None, None
                     size: dp(400), dp(400)
                     pos_hint: {"center_y": 0.42, "x": 0.40}
                     on_release: app.root.current = 'camera'
-                    
+
                 MDLabel:
                     text: "SET 3"
                     halign: "center"
@@ -452,7 +463,7 @@ ScreenManager:
                     size: dp(350), dp(350)
                     pos_hint: {"center_y": 0.33, "x": 0.62}
                     on_release: app.root.current = 'camera'
-                
+
                 MDLabel:
                     text: "SET 4"
                     halign: "center"
@@ -464,25 +475,27 @@ ScreenManager:
             anchor: 'right'
             md_bg_color: 146/255, 170/255, 131/255, 1
 
-            BoxLayout:
-                orientation: 'vertical'
-                spacing: dp(20)
-                padding: dp(20) 
 
             MDList:
                 OneLineListItem:
                     text: "ACCOUNT"
                     theme_text_color: "Custom"
                     text_color: "black"
-                    on_release: app.root.current = 'account'
-                    
-                    
-            MDList:
+                    size_hint_y: None
+                    on_release: app.root.current = 'account'    
+
                 OneLineListItem:
                     text: "ABOUT"
                     theme_text_color: "Custom"
                     text_color: "black"
-                    on_release: app.root.current = 'about'        
+                    on_release: app.root.current = 'about'      
+
+                OneLineListItem:
+                    text: "CONTACT"
+                    theme_text_color: "Custom"
+                    text_color: "black"
+                    on_release: app.root.current = 'contact'            
+
 
 <CameraPage>:
     name: 'camera'
@@ -492,7 +505,7 @@ ScreenManager:
         Rectangle:
             size: self.size
             pos: self.pos   
-            
+
 <AccountPage>:
     name: 'account'
     canvas.before:
@@ -501,25 +514,34 @@ ScreenManager:
         Rectangle:
             size: self.size
             pos: self.pos
-    
+
+    MDIconButton:
+        icon: "arrow-left"
+        theme_text_color: "Custom"
+        text_color: 0, 0, 0, 1
+        size_hint: None, None
+        size: dp(50), dp(50)
+        pos_hint: {"x": 0.95, "top": 0.98}
+        on_release: app.root.current = 'home'
+
     BoxLayout:
         orientation: 'horizontal'
         size_hint_y: None
         height: 120
         padding: 20
         spacing: 15
-        
+
         Image:
             source: 'Profile.png'
             size_hint: None, None
             size: 60, 60
             pos_hint: {"y": 11, "x": 3}
-            
+
         FloatLayout:
             orientation: 'vertical'
             size_hint_y: None
             height: 60
-            
+
             Label:
                 text: "FULL NAME"
                 bold: True
@@ -527,14 +549,14 @@ ScreenManager:
                 color: 0, 0, 0, 1
                 height: 25
                 pos_hint: {"center_x": 0.02, "center_y": 15.1}
-            
+
             Label:
                 text: "@USERNAME"
                 font_size: 14
                 color: 0, 0, 0.5, 1
                 height: 20
                 pos_hint: {"center_x": 0.02, "center_y": 14.8}
-            
+
             Label:
                 text: "PHOTOS"
                 font_size: 14
@@ -552,35 +574,125 @@ ScreenManager:
         Rectangle:
             size: self.size
             pos: self.pos  
-    
+
+    MDIconButton:
+        icon: "arrow-left"
+        theme_text_color: "Custom"
+        text_color: 0, 0, 0, 1
+        size_hint: None, None
+        size: dp(50), dp(50)
+        pos_hint: {"x": 0.95, "top": 0.98}
+        on_release: app.root.current = 'home'
+
     BoxLayout:
         orientation: 'horizontal'
         size_hint_y: None
         height: 120
         padding: 20
-        spacing: 15
-            
+        spacing: 15 
+
         Image:
             source: 'About.png'
             size_hint: None, None
-            size: 80, 80
-            pos_hint: {"y": 11, "x": 3}
-                
-        FloatLayout:
-            orientation: 'vertical'
+            size: 60, 60
+            pos_hint: {"y": 10.8, "x": 3}               
+
+    FloatLayout:
+        orientation: 'vertical'
+        size_hint_y: None
+        height: 60
+
+        Label:
+            text: "ABOUT US"
+            bold: True
+            font_size: 25
+            color: 0, 0, 0, 1
             size_hint_y: None
-            height: 60
-            
-            Label:
-                text: "ABOUT US"
-                bold: True
-                font_size: 18
-                color: 0, 0, 0, 1
-                size_hint_y: None
-                height: 30       
-                pos_hint: {"center_x": 0.02, "center_y": 15.1}
-            
-              
+            height: 30       
+            pos_hint: {"center_x": 0.08, "center_y": 15.2}
+
+
+
+<ContactPage>:
+    name: 'contact'
+    canvas.before:
+        Color:
+            rgba: (240/255, 246/255, 237/255, 1)  # Light green background
+        Rectangle:
+            size: self.size
+            pos: self.pos  
+
+    MDIconButton:
+        icon: "arrow-left"
+        theme_text_color: "Custom"
+        text_color: 0, 0, 0, 1
+        size_hint: None, None
+        size: dp(50), dp(50)
+        pos_hint: {"x": 0.95, "top": 0.98}
+        on_release: app.root.current = 'home'
+
+    BoxLayout:
+        orientation: 'horizontal'
+        size_hint_y: None
+        height: 120
+        padding: 20
+        spacing: 15      
+
+
+    FloatLayout:
+        orientation: 'vertical' 
+        size_hint_y: None
+        height: 60
+
+        Label:
+            text: "CONTACTS"
+            bold: True
+            font_size: 25
+            color: 0, 0, 0, 1
+            height: 40   
+            pos_hint: {"center_x": 0.05, "center_y": 15.0}      
+
+        Image:
+            source: 'Email.png'
+            size_hint: None, None
+            size: 40, 40
+            pos_hint: {"y": 14.0, "x": 0.02}
+
+        Label:
+            text: "shutterboothofficial@gmail.com"
+            bold: True
+            font_size: 15
+            color: 0, 0, 0, 1
+            height: 40   
+            pos_hint: {"center_x": 0.10, "center_y": 14.3}   
+
+        Image:
+            source: 'Facebook.png'
+            size_hint: None, None
+            size: 40, 40
+            pos_hint: {"y": 13.2, "x": 0.02}  
+
+        Label:
+            text: "Shutter Booth Official"
+            bold: True
+            font_size: 15
+            color: 0, 0, 0, 1
+            height: 40   
+            pos_hint: {"center_x": 0.08, "center_y": 13.5}  
+
+        Image:
+            source: 'Contact.png'
+            size_hint: None, None
+            size: 40, 40
+            pos_hint: {"y": 12.3, "x": 0.02}  
+
+        Label:
+            text: "(08)- 2025 -1709"
+            bold: True
+            font_size: 15
+            color: 0, 0, 0, 1
+            height: 40   
+            pos_hint: {"center_x": 0.08, "center_y": 12.7}                   
 '''
 
 
